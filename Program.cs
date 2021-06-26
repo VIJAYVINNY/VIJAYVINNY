b2ae9082793a86f7957c2a41308d46ed1532db98
Vijay kumar <70283128+VIJAYVINNY@users.noreply.github.com>
2021-06-27 00:27:27 +0530

    Delegate
    commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegateProject
+{
+    public delegate void Adddelegate(int x, int y);
+    public delegate string saydelegates(string str);
+    class Program
+    {
+        public void AddNums(int a,int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static  string sayHello(string name)
+        {
+            return "Hello" + name;
+        }
+        static void Main()
+        {
+            Program p = new Program();
+            Adddelegate ad = new Adddelegate(p.AddNums);
+            ad.Invoke(122, 122);
+            saydelegates sd = new saydelegates(Program.sayHello);
+            string str = sd.Invoke("Raju");
+        }
+       
+    }
+}
